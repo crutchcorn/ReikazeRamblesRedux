2fab3abf47e6e8ec5fe6caf50aedfe7ec0cdf92e
Corbin Crutchley <git@crutchcorn.dev>
2024-08-30 19:45:37 -0700

    chore: deploy with GH pages

--- astro.config.ts
+++ astro.config.ts
@@ -5,26 +5,15 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { EnumChangefreq as ChangeFreq } from "sitemap";
 import { siteUrl } from "./src/constants/site-config";
-import vercel from "@astrojs/vercel/static";
 import symlink from "symlink-dir";
 import * as path from "path";
-import { SUPPORTED_IMAGE_SIZES } from "./src/utils/get-picture";
 import { astroIntegrationCopyGenerated } from "./src/utils/markdown/astro-integration-copy-generated";
 
 await symlink(path.resolve("content"), path.resolve("public/content"));
 
 export default defineConfig({
 	site: siteUrl,
-	adapter: vercel({
-		// Uses Vercel's Image Optimization API: https://vercel.com/docs/image-optimization
-		imageService: true,
-		imagesConfig: {
-			sizes: SUPPORTED_IMAGE_SIZES,
-			domains: [],
-			formats: ["image/avif", "image/webp"],
-		},
-		devImageService: "sharp",
-	}),
+	output: "static",
 	integrations: [
 		icon(),
 		preact({ compat: true }),
